31964ef3f251eebd874a9f92cbf60c7a6f9d79a3
Marko Mitranic <marko.mitranic@catenamedia.com>
2019-05-23 21:29:21 +0200

    Create 4th Step

--- Docker/php/entrypoint.sh
+++ Docker/php/entrypoint.sh
@@ -13,4 +13,9 @@ COMPOSER_ALLOW_SUPERUSER=1 composer install
 APP_ENV=dev APP_DEBUG=1 php bin/console cache:clear
 echo "[Operation] Composer deployment finished for step3."
 
+cd /usr/share/nginx/app/step4/website-skeleton/
+COMPOSER_ALLOW_SUPERUSER=1 composer install
+APP_ENV=dev APP_DEBUG=1 php bin/console cache:clear
+echo "[Operation] Composer deployment finished for step4."
+
 php-fpm
---
+++ public/step4/website-skeleton/src/Controller/IndexController.php
@@ -0,0 +1,45 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\AuthorFilterType;
+use App\Repository\AuthorsRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Contracts\Cache\ItemInterface;
+
+/**
+ * Class IndexController
+ * @package App\Controller
+ */
+class IndexController extends AbstractController
+{
+
+    /**
+     * @Route("/step3/")
+     */
+    public function list()
+    {
+        return $this->render('authors/authors.html.twig');
+    }
+
+    /**
+     * @Route("/step3/user/{id}")
+     */
+    public function findUserApi(AuthorsRepository $repository, string $id)
+    {
+        $authorCache = new FilesystemAdapter('author', 3600);
+
+        $author = $authorCache->get($id, function (ItemInterface $item) use ($repository, $id){
+            $item->expiresAfter(3600);
+            $author = $repository->find($id);
+            return $author;
+        });
+
+        return new JsonResponse($author);
+    }
+
+}
