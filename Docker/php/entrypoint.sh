be740f8dd7ac8b04d96c0558f729f19600ddef39
Marko Mitranic <marko.mitranic@catenamedia.com>
2019-05-23 19:09:31 +0200

    Automatically install composer and trigger it as a build step

--- Docker/php/entrypoint.sh
+++ Docker/php/entrypoint.sh
@@ -3,4 +3,9 @@ set -e
 
 /etc/init.d/blackfire-agent restart
 
+cd /usr/share/nginx/app/step2/website-skeleton/
+COMPOSER_ALLOW_SUPERUSER=1 composer install
+APP_ENV=dev APP_DEBUG=1 php bin/console cache:clear
+echo "[Operation] Composer deployment finished for step2.
+
 php-fpm
