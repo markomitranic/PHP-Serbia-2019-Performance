dbac3743f760dd8085b57dc355efcd66fb60085c
Marko Mitranic <marko.mitranic@catenamedia.com>
2019-05-23 19:33:13 +0200

    Patch up a syntax error on deploy script

--- Docker/php/entrypoint.sh
+++ Docker/php/entrypoint.sh
@@ -6,6 +6,6 @@ set -e
 cd /usr/share/nginx/app/step2/website-skeleton/
 COMPOSER_ALLOW_SUPERUSER=1 composer install
 APP_ENV=dev APP_DEBUG=1 php bin/console cache:clear
-echo "[Operation] Composer deployment finished for step2.
+echo "[Operation] Composer deployment finished for step2."
 
 php-fpm
